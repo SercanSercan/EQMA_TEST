86c194825ebe3072127303ec28fe1c2a46e08ef2
Sercan Leylek <sercan.leylek@fremtind.no>
2025-04-12 08:34:01 +0200

    Add google fonts and search text input

--- src/components/LatestEarthQuakes/LatestEarthQuakes.tsx
+++ src/components/LatestEarthQuakes/LatestEarthQuakes.tsx
@@ -1,12 +1,24 @@
 import './LatestEarthQuakes.scss';
 import { IEarthQuakeLog, ILatestEarthQuakes } from "../../utilities/interfaces.ts";
+import { TextInput } from "@fremtind/jokul";
+import { useState } from "react";
 
 const LatestEarthQuakes: React.FC<ILatestEarthQuakes> = ({ allEarthquakes }) => {
+    const [searchText, setSearchText] = useState('');
+
     return (
         <div className="latestEarthQuakes">
+            <TextInput
+                label=""
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+                name="search-box"
+                placeholder="Search..."
+                labelProps={{ variant: "small" }}
+            />
             {allEarthquakes.slice(0, 5).map((earthquake: IEarthQuakeLog, idx) => {
                 const { Location } = earthquake;
-                return (<div>{Location}</div>);
+                return (<div key={idx}>{Location}</div>);
             })}
         </div>
     );
