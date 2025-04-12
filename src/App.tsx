23abc4e40987b00d967c225c742a744fb771e17e
Sercan Leylek <sercan.leylek@fremtind.no>
2025-04-12 19:17:20 +0200

    Show error message when system is down

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import { getEqLogs } from "./utilities/api.ts";
 import { IEarthQuakeLog } from "./utilities/interfaces.ts";
 import LatestEarthQuakes from "./components/LatestEarthQuakes/LatestEarthQuakes.tsx";
 import TopFive from "./components/TopFive/TopFive.tsx";
-import { Loader } from "@fremtind/jokul";
+import { ErrorMessage, Loader } from "@fremtind/jokul";
 
 function App() {
-    const [loading, setLoading] = useState<boolean>(true);
     const [allEarthquakes, setAllEarthquakes] = useState<IEarthQuakeLog[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [isError, setIsError] = useState<boolean>(false);
 
     useEffect(() => {
         (async ()=> {
@@ -17,6 +18,7 @@ function App() {
                 setAllEarthquakes(logs);
             } catch (err) {
                 console.error(err);
+                setIsError(true);
             } finally {
                 setLoading(false);
             }
@@ -26,16 +28,23 @@ function App() {
     return (
         <div className="app">
             <h1>Eartquake Monitoring App (EQMA)</h1>
-            {loading ? (
+            {loading && (
                 <Loader
                     variant="large"
                     textDescription="Fetching EQMA Logs"
                 />
-            ): (
+            )}
+            {!loading && !isError && (
                 <div className="app__cards">
                     <LatestEarthQuakes allEarthquakes={allEarthquakes} />
                     <TopFive allEarthquakes={allEarthquakes} />
                 </div>
+            )}
+            {isError && (
+                <ErrorMessage
+                    fullWidth={false}
+                    dismissed={false}
+                >Our service is down 😔</ErrorMessage>
             )}
         </div>
     );
