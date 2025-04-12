4850b6569c49342e1707d67e5c95b14a99b08ab1
Sercan Leylek <sercan.leylek@fremtind.no>
2025-04-12 19:31:08 +0200

    Show 20 latest eqs

--- src/components/LatestEarthQuakes/LatestEarthQuakes.tsx
+++ src/components/LatestEarthQuakes/LatestEarthQuakes.tsx
@@ -1,13 +1,14 @@
 import './LatestEarthQuakes.scss';
 import { IEarthQuakeLog, ILatestEarthQuakes } from "../../utilities/interfaces.ts";
 import LogRow from "../LogRow/LogRow.tsx";
+import { LIMIT } from "../../utilities/constants.ts";
 
 const LatestEarthQuakes: React.FC<ILatestEarthQuakes> = ({ allEarthquakes }) => {
     return (
         <div className="latestEarthQuakes">
             <h2>Latest Earthquakes in last 24h</h2>
             <div className="latestEarthQuakes__list">
-                {allEarthquakes.slice(0, 5).map((earthquake: IEarthQuakeLog, idx) => (
+                {allEarthquakes.slice(0, LIMIT).map((earthquake: IEarthQuakeLog, idx) => (
                     <LogRow earthquakeLog={earthquake} key={idx}/>
                 ))}
             </div>
