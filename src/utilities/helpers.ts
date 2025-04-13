1a29ce1c9600589ed54026ef9144d8739ffa9e39
Sercan Leylek <sercan.leylek@fremtind.no>
2025-04-13 16:01:44 +0200

    Convert custom date string to valid format

--- src/utilities/helpers.ts
+++ src/utilities/helpers.ts
@@ -1,5 +1,6 @@
 export const formatDate = (dateStr: string): string => {
-    const date = new Date(dateStr);
+    const date = new Date(dateStringToISO(dateStr));
+
     const month = date.toLocaleString('en-EN', { month: 'long' });
     const day = date.getDate();
     const hours = date.getHours().toString().padStart(2, '0');
@@ -7,6 +8,10 @@ export const formatDate = (dateStr: string): string => {
 
     return `${month} ${day}, ${hours}:${minutes}`;
 };
+
+const dateStringToISO = (dateStr: string) => {
+    return dateStr.replace(" ", "T").replace(/\./g,'-');
+}
 
 export const compare = (a: number, b: number) => {
     if (a < b) {
